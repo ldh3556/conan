4e9e0d1461c317e818c64300ade01a8e760d0c45
haku <hdh1084@gmail.com>
2025-01-23 18:33:24 +0900

    "worldcup rank" tried but stopped trial

--- src/main/webapp/sql/DB_Rank.sql
+++ src/main/webapp/sql/DB_Rank.sql
@@ -1,10 +1,14 @@
-CREATE TABLE winner_table_hdh (
-    id INT auto_increment PRIMARY KEY,  -- 순위를 구분할 수 있도록 id 추가
-    name VARCHAR(50) NOT NULL,
-    file VARCHAR(100) NOT NULL,
-    rank INT NOT NULL,  -- 우승자 순위를 나타내는 rank 컬럼
-    ratio DECIMAL(5,2) DEFAULT 0.00
+create table worldcup_rank (
+    person_id number(3) primary key,      -- 인물의 pk
+    person_name varchar2(50) not null,   -- 인물 이름
+    win_count number(4) default 0       -- 인물의 우승횟수
 );
 
--- 우승자 저장 예시
-insert into winner_table_hdh (id, name, file, rank, ratio) values (?, '브라운', '/img/vote/worldcup/men/agasa.png', ?, 100.00);
+create sequence worldcup_rank_seq;
+insert into worldcup_rank (person_id, person_name) values (worldcup_rank_seq.nextval,'브라운');
+
+
+
+
+
+select * from worldcup_rank;
